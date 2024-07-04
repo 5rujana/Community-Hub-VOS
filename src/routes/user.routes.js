903ced997d037194aaaf723036bd3bed08d26c99
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-07-04 12:37:23 +0530

    feat: Add routes for changing password, updating account details, and managing user profile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,17 @@ import { registerUser } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import {loginUser,logoutUser,refreshAccessToken} from "../controllers/user.controllers.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
+import {registerUser,
+        loginUser,
+        logoutUser,
+        refreshAccessToken,
+        changeCurrentPassword,
+        updateAccountDetails,
+        getCurrentUser,
+        updateUserAvatar,
+        updateUserCoverImage,
+        getUserAccountProfile} from "../controllers/user.controllers.js"
+
 
 const router = Router()
 
@@ -24,6 +35,11 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserAccountProfile)
 
 export default router
