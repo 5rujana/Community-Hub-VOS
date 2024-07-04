a1fbafb2f9d16e46733a81b8fe446ecbbbff6218
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-07-04 13:04:11 +0530

    feat: Import app as a named import in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 // import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js"
 import connectDB from "./db/index.js"
-import app from "./app.js"
+import {app} from "./app.js"
 
 dotenv.config({
     path:'./env'
