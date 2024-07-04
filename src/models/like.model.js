906fd9acce0e62c029e8404cfc13cf4eaa70a028
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-07-04 12:35:50 +0530

    feat: Add like model for storing likes

---
+++ src/models/like.model.js
@@ -0,0 +1,24 @@
+import mongoose, {Schema} from "mongoose";
+
+
+const likeSchema = new Schema({
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    },
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+    },
+    thread: {
+        type: Schema.Types.ObjectId,
+        ref: "Thread"
+    },
+    likedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    
+}, {timestamps: true})
+
+export const Like = mongoose.model("Like", likeSchema)
