b472fd6fc00a68b75a9280b16404de920d6fa613
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-07-04 12:37:13 +0530

    feat: Add thread routes for creating, updating, and deleting threads

---
+++ src/routes/thread.routes.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import {
+    createThread,
+    getUserThreads,
+    updateThread,
+    deleteThread
+} from "../controllers/thread.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router = Router();
+router.use(verifyJWT);
+
+router.route("/").post(createThread);
+router.route("/user/:userId").get(getUserThreads);
+router.route("/:threadId").patch(updateThread).delete(deleteThread);
+
+export default router
