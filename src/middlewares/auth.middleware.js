4f9b787be75f52d75de094ec693778d8087193d7
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-07-04 12:35:23 +0530

    refactor: Update import statement for asyncHandler in auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-import { asyncHandler } from "../utils/asynchandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiErros.js"
 import jwt from "jsonwebtoken";
 import {User} from "../models/user.model.js"
