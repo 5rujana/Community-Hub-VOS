81973c295a57473a9578c912ac2593ca38470422
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-07-04 13:04:27 +0530

    fix: Correct import typo in auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiErros.js"
+import { ApiError } from "../utils/ApiError.js"
 import jwt from "jsonwebtoken";
 import {User} from "../models/user.model.js"
 
