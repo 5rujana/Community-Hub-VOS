3abd836eb44a66cee671094888b75779931818f4
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-09-30 19:27:28 +0530

    Refactor package-lock.json and feed.jsx
    - Remove "license" field from package-lock.json
    - Update API endpoint in feed.jsx
    - Fix import paths in db/index.js and user.controllers.js

--- src/backend/src/db/index.js
+++ src/backend/src/db/index.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import {DB_NAME} from "../../../constants.js"
+import {DB_NAME} from "../constants.js"
 
 const connectDB = async () =>{
     try{
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI }  /${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOSTED AT ${connectionInstance.connection.host} \n`);
     }catch(error){
         console.error("MongoDB connection error",error);
